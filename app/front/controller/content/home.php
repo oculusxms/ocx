8b42ef029f35b5859ed7ff0a18de1d703aa3a4b3
Vince Kronlein <vkronlein@icloud.com>
2014-12-30 09:13:22 -0700

    codeformatter errors
    Signed-off-by: Vince Kronlein <vkronlein@icloud.com>

--- app/front/controller/content/home.php
+++ app/front/controller/content/home.php
@@ -103,72 +103,69 @@ class Home extends Controller {
             foreach ($posts as $post):
                 if ($post['image']):
                     $image = IMAGE_URL . $post['image'];
-                     // remove resizing and allow img-responsive to handle sizing
-                    else:
-                        $image = '';
-                    endif;
-                    
-                    if ($this->config->get('blog_comment_status')):
-                        $rating = (int)$post['rating'];
-                    else:
-                        $rating = false;
-                    endif;
-                    
-                    $categories = $this->model_content_category->getCategoriesByPostId($post['post_id']);
-                    
-                    $posted_in = array();
-                    $posted_in_categories = '';
-                    
-                    if ($categories):
-                        foreach ($categories as $category):
-                            $posted_in[] = sprintf($this->language->get('text_posted_categories'), $category['href'], $category['name']);
-                        endforeach;
-                    endif;
-                    
-                    if (!empty($posted_in)):
-                        $posted_in_categories = implode(", ", $posted_in);
-                    endif;
-                    
-                    $comment_text = ($post['comments'] == 1) ? rtrim($this->language->get('text_comments'), 's') : $this->language->get('text_comments');
-                    
-                    $data['posts'][] = array('post_id' => $post['post_id'], 'author_name' => $post['author_name'], 'thumb' => $image, 'name' => $post['name'], 'short' => utf8_substr(strip_tags(html_entity_decode($post['description'], ENT_QUOTES, 'UTF-8')), 0, 450) . '..', 'blurb' => utf8_substr(strip_tags(html_entity_decode($post['description'], ENT_QUOTES, 'UTF-8')), 0, 200) . '..', 'rating' => $rating, 'views' => sprintf($this->language->get('text_views'), (int)$post['viewed']), 'comments' => sprintf($comment_text, (int)$post['comments']), 'href' => $this->url->link('content/post', 'post_id=' . $post['post_id']), 'comments_href' => $this->url->link('content/post', 'post_id=' . $post['post_id'] . '&to_comments=1'), 'author_href' => $this->url->link('content/search', '&filter_author_id=' . $post['author_id']), 'date_added' => date($this->language->get('post_date'), strtotime($post['date_added'])), 'categories' => $posted_in_categories);
-                endforeach;
-                
-                $url = '';
-                
-                if (isset($this->request->get['sort'])):
-                    $url.= '&sort=' . $this->request->get['sort'];
-                endif;
-                
-                if (isset($this->request->get['order'])):
-                    $url.= '&order=' . $this->request->get['order'];
-                endif;
-                
-                if (isset($this->request->get['limit'])):
-                    $url.= '&limit=' . $this->request->get['limit'];
-                endif;
-                
-                $data['pagination'] = $this->theme->paginate($post_total, $page, $limit, $this->language->get('text_pagination'), $this->url->link('content/home', $url . '&page={page}'));
-                
-                $data['sort'] = $sort;
-                $data['order'] = $order;
-                $data['limit'] = $limit;
-                
-                // Search
-                if (isset($this->request->get['filter_name'])):
-                    $data['filter_name'] = $this->request->get['filter_name'];
+                    // remove resizing and allow img-responsive to handle sizing
                 else:
-                    $data['filter_name'] = '';
+                    $image = '';
                 endif;
                 
-                $data = $this->theme->listen(__CLASS__, __FUNCTION__, $data);
+                if ($this->config->get('blog_comment_status')):
+                    $rating = (int)$post['rating'];
+                else:
+                    $rating = false;
+                endif;
                 
-                $data = $this->theme->render_controllers($data);
+                $categories = $this->model_content_category->getCategoriesByPostId($post['post_id']);
                 
-                $this->response->setOutput($this->theme->view('content/home', $data));
+                $posted_in = array();
+                $posted_in_categories = '';
+                
+                if ($categories):
+                    foreach ($categories as $category):
+                        $posted_in[] = sprintf($this->language->get('text_posted_categories'), $category['href'], $category['name']);
+                    endforeach;
+                endif;
+                
+                if (!empty($posted_in)):
+                    $posted_in_categories = implode(", ", $posted_in);
+                endif;
+                
+                $comment_text = ($post['comments'] == 1) ? rtrim($this->language->get('text_comments'), 's') : $this->language->get('text_comments');
+                
+                $data['posts'][] = array('post_id' => $post['post_id'], 'author_name' => $post['author_name'], 'thumb' => $image, 'name' => $post['name'], 'short' => utf8_substr(strip_tags(html_entity_decode($post['description'], ENT_QUOTES, 'UTF-8')), 0, 450) . '..', 'blurb' => utf8_substr(strip_tags(html_entity_decode($post['description'], ENT_QUOTES, 'UTF-8')), 0, 200) . '..', 'rating' => $rating, 'views' => sprintf($this->language->get('text_views'), (int)$post['viewed']), 'comments' => sprintf($comment_text, (int)$post['comments']), 'href' => $this->url->link('content/post', 'post_id=' . $post['post_id']), 'comments_href' => $this->url->link('content/post', 'post_id=' . $post['post_id'] . '&to_comments=1'), 'author_href' => $this->url->link('content/search', '&filter_author_id=' . $post['author_id']), 'date_added' => date($this->language->get('post_date'), strtotime($post['date_added'])), 'categories' => $posted_in_categories);
+            endforeach;
+                
+            $url = '';
+            
+            if (isset($this->request->get['sort'])):
+                $url.= '&sort=' . $this->request->get['sort'];
             endif;
-        }
+            
+            if (isset($this->request->get['order'])):
+                $url.= '&order=' . $this->request->get['order'];
+            endif;
+            
+            if (isset($this->request->get['limit'])):
+                $url.= '&limit=' . $this->request->get['limit'];
+            endif;
+            
+            $data['pagination'] = $this->theme->paginate($post_total, $page, $limit, $this->language->get('text_pagination'), $this->url->link('content/home', $url . '&page={page}'));
+            
+            $data['sort'] = $sort;
+            $data['order'] = $order;
+            $data['limit'] = $limit;
+            
+            // Search
+            if (isset($this->request->get['filter_name'])):
+                $data['filter_name'] = $this->request->get['filter_name'];
+            else:
+                $data['filter_name'] = '';
+            endif;
+            
+            $data = $this->theme->listen(__CLASS__, __FUNCTION__, $data);
+            
+            $data = $this->theme->render_controllers($data);
+            
+            $this->response->setOutput($this->theme->view('content/home', $data));
+        endif;
     }
 }
-
-    
