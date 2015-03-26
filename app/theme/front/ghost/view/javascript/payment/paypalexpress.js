1912c12b290f0e768f6d97b1b8bf0a83cb9eef15
Vince Kronlein <vkronlein@icloud.com>
2015-03-25 18:59:33 -0700

    express js error
    Signed-off-by: Vince Kronlein <vkronlein@icloud.com>

--- app/theme/front/ghost/view/javascript/payment/paypalexpress.js
+++ app/theme/front/ghost/view/javascript/payment/paypalexpress.js
@@ -1,6 +1,6 @@
 <script>
 $(document).on('click', '#button-confirm', function(e){
 	e.preventDefault();
-	location = '<?= $lang_button_continue_action; ?>';
+	location = '<?= $button_continue_action; ?>';
 });
 </script>
